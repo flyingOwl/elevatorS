9c75c197ff8e7c36be961167ee3c3de13f4d6595
flyingOwl <no@email>
2013-02-24 16:05:09 +0100

    Simplified main.c a bit

--- main.c
+++ main.c
@@ -17,18 +17,18 @@ void * startCoreLoop(void *ptr){
 
 int main(int argc, char ** argv){	
 	int * pArgs = malloc(sizeof(int) * 6);
-	pArgs[0] = 15;    //every x steps -> new passenger
-	pArgs[1] = 8;     //house size (levels)
-	pArgs[2] = 1000;  //malloc for waiters
-	pArgs[3] = 12;    //elevator size (maxPassengers)
-	pArgs[4] = 4;     //speed steps (1/x per second)
-	pArgs[5] = 25;   //Core loop pause time in ms
+	pArgs[0] = 15;		//every x steps -> new passenger
+	pArgs[1] = 8;		//house size (levels)
+	pArgs[2] = 1000;	//malloc for waiters
+	pArgs[3] = 12;		//elevator size (maxPassengers)
+	pArgs[4] = 4;		//speed steps (1/x per second)
+	pArgs[5] = 25;		//Core loop pause time in ms
 	pthread_t pCore;
 	pthread_create(&pCore, NULL, startCoreLoop, pArgs);
 
 	int * pFac = malloc(sizeof(int) * 2);
-	pFac[0] = 25;    //loop pause time in ms
-	pFac[1] = 8;      //house size (levels)
+	pFac[0] = pArgs[5];	//loop pause time in ms
+	pFac[1] = pArgs[1];	//house size (levels)
 	pthread_t pInterface;
 	pthread_create(&pInterface, NULL, startInterfaceLoop, pFac);
 	
